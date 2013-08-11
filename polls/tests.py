284fe3baae96c35ea329758b8600b95e0fbe4a2f
suma <ddllsuma@gmail.com>
2013-08-12 02:15:20 +0800

    create a test to expose the bug

--- polls/tests.py
+++ polls/tests.py
@@ -5,12 +5,16 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
+from django.utils import timezone
 from django.test import TestCase
 
+from polls.models import poll
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class PollMethodTests(TestCase):
+  def test_was_published_recently_with_future_poll(self):
+    """
+    was_pulished_recently() should return False for polls whose
+    pub_date is in the future
+    """
+    future_poll = Poll(pub_date=timezone.now() + datetime.timezone.timedelta(days=30))
+    self.assertEqual(future_poll.was_published_recently(), False)
