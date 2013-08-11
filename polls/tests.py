cb739c3216fe6b9604d8aa54fa87b1acca3c1aa6
suma <ddllsuma@gmail.com>
2013-08-12 02:56:08 +0800

    make *.pyc file unstaged

--- polls/tests.py
+++ polls/tests.py
@@ -5,10 +5,11 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
+import datetime
 from django.utils import timezone
 from django.test import TestCase
 
-from polls.models import poll
+from polls.models import Poll
 
 class PollMethodTests(TestCase):
   def test_was_published_recently_with_future_poll(self):
@@ -16,5 +17,5 @@ class PollMethodTests(TestCase):
     was_pulished_recently() should return False for polls whose
     pub_date is in the future
     """
-    future_poll = Poll(pub_date=timezone.now() + datetime.timezone.timedelta(days=30))
+    future_poll = Poll(pub_date=timezone.now() + datetime.timedelta(days=30))
     self.assertEqual(future_poll.was_published_recently(), False)
