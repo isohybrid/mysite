89a966792eaa725b9e3251956ff7a39a6b532171
suma <ddllsuma@gmail.com>
2013-08-18 18:27:28 +0800

    add django-admin-bootstrapped

--- mysite/settings.py
+++ mysite/settings.py
@@ -72,7 +72,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/data/site/mysite/static"
+    "/data/site/mysite/static",
 )
 
 # List of finder classes that know how to find static files in
@@ -123,6 +123,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_admin_bootstrapped',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
