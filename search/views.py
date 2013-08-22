63860186397e06022d4e4b68a566bb7c4419333c
suma <ddllsuma@gmail.com>
2013-08-23 01:54:38 +0800

    improve the search view

--- search/views.py
+++ search/views.py
@@ -2,11 +2,19 @@ from django.http import HttpResponse
 from django.template import loader, Context
 from django.contrib.flatpages.models import FlatPage
 
+from django.shortcuts import render_to_response
+
 # Create your views here.
 def search(request):
-  query = request.GET['q']
-  results = FlatPage.objects.filter(content__icontains=query)
-  template = loader.get_template('search/search.html')
-  context = Context({ 'query': query, 'results': results })
-  response = template.render(context)
-  return HttpResponse(response)
+  query = request.GET['q', '']
+  # results = FlatPage.objects.filter(content__icontains=query)
+  # template = loader.get_template('search/search.html')
+  # context = Context({ 'query': query, 'results': results })
+  # response = template.render(context)
+  # return HttpResponse(response)
+  results = []
+  if query:
+    results= FlatPage.objects.filter(content__icontains=query)
+  return render_to_response('search/search.html',
+      { 'query': query,
+        'results': results })
